f77d36885dba2a122f87eab37d2724ba4d88ec77
Tomoya Watanabe <tomoya.watanabe@firstloop-tech.com>
2020-11-14 21:33:15 +0900

    convert to onnx and coreml

--- src/save_coreml.py
+++ src/save_coreml.py
@@ -8,14 +8,18 @@ import argparse
 import inspect
 
 parser = argparse.ArgumentParser(description='')
-parser.add_argument('-i', '--image_path', type=str)
 parser.add_argument('-o', '--onnx_file', type=str)
+parser.add_argument('-f', '--saved_feature_path', type=str)
 args = parser.parse_args()
-image_path = args.image_path
 onnx_file = args.onnx_file
+saved_feature_path = args.saved_feature_path
 
-example_image = Image.open("../example/000001.png")
-example_image = example_image.resize((128, 128))
+saved_feature = np.loadtxt(saved_feature_path).reshape(-1)
+
+example_image01 = Image.open("../example/000001.png")
+example_image01 = example_image01.resize((128, 128))
+example_image02 = Image.open("../example/000002.jpg")
+example_image02 = example_image02.resize((128, 128))
 coreml_file = onnx_file + ".mlmodel"
 
 scale = 1.0 / (0.5 * 255.0)
@@ -31,5 +35,15 @@ model = ct.converters.onnx.convert(
     model=onnx_file, image_input_names=["input_1"], preprocessing_args=preprocessing_args)
 model.save(coreml_file)
 
-out_dict = model.predict({"input_1": example_image})
-print(out_dict)
+out_dict01 = model.predict({"input_1": example_image01})
+embedding01 = out_dict01["output_1"].reshape(-1)
+out_dict02 = model.predict({"input_1": example_image02})
+embedding02 = out_dict02["output_1"].reshape(-1)
+
+# cosine similality
+cos_sim = np.dot(saved_feature, embedding01) / \
+    (np.linalg.norm(saved_feature, ord=2) * np.linalg.norm(embedding01, ord=2))
+print(f"Image01: {cos_sim}")
+cos_sim = np.dot(saved_feature, embedding02) / \
+    (np.linalg.norm(saved_feature, ord=2) * np.linalg.norm(embedding02, ord=2))
+print(f"Image02: {cos_sim}")
